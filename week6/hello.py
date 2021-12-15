53369129b5058293d10051f29c441075e3d28342
mxw035 <68867749+mxw035@users.noreply.github.com>
2021-12-14 17:01:22 -0700

    Create hello.py

---
+++ week6/hello.py
@@ -0,0 +1,5 @@
+from cs50 import get_string  # imports get_string fxn from the cs50 libarary
+
+h = get_string("What is your name? ")  # creates a variable called h that gets a string input from user
+
+print(f"Hello, {h}")  # uses the python print f fxn to print a response to user, using their input
